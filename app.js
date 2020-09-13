44861af33901eba6b481cb79318c60658205dd0e
Ludvig Lind <ll223vs@student.lnu.se>
2020-09-13 12:45:05 +0200

    Implement fullPyramid

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import * as build from "./pyramid-builder.js"
 
-console.log('Greetings, builder!')
+console.log('Greetings Builder!')
+console.log('What sort of pyramid would you like to create?')
+
 console.log(build.rightAngledPyramid(7))
 console.log(build.leftAngledPyramid(7))
 console.log(build.fullPyramid(7))
